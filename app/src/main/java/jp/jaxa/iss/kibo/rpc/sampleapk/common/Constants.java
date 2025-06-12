4c7d9739ce8fb35750046efd371427af45091bf5
FHRC <fozlulcoc1592@gmail.com>
2025-06-12 19:56:16 +0600

    added constants

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/common/Constants.java
@@ -0,0 +1,12 @@
+public class Constants {
+    public static final int mx_rt = 3;
+    public static final int rt_wait_ms = 500;
+    public static final int mv_to_wait = 1500;
+    public static final int cam_stab_wait = 2000;
+    public static final boolean dbg_mode = false;
+
+    public static final List<AreaEnum> area_list;
+    public static final Map<AreaEnum, QuaternionPoint> lt_item_srch_parea;
+    
+
+}
